65183688f8a44e55798461a32854529254809674
Leslie Jaiyesimi <lesjaiye@comcast.net>
2026-03-24 18:44:03 -0400

    signUp static pages

--- app/onboard/login/page.tsx
+++ app/onboard/login/page.tsx
@@ -1,16 +1,17 @@
-
 'use client';
 import { useRouter } from 'next/navigation';
 
-export default function Signup() {
+export default function Login() {
   const router = useRouter();
   return (
     <div className="flex flex-col gap-8">
         <div className="flex flex-col gap-2">
         <input type="email" id="email" placeholder="Email" className="border border-black rounded-md p-2 placeholder:text-gray-400" />
         <input type="password" id="password" placeholder="Password" className="border border-black rounded-md p-2 placeholder:text-gray-400" />
+        <p className="text-blue-500 text-right text-sm">Forgot password?</p>
         </div>
-      <button className="bg-gray-200 text-black rounded-md p-2">Sign Up</button>
+      <button className="bg-gray-200 text-black rounded-md p-2">Sign In</button>
+      <p className="text-blue-500 text-right text-sm">Don't have an account? <button onClick={() => router.push('/onboard/signup')} className="text-blue-500 underline">Sign Up</button></p>
     </div>
   );
 }
--- app/onboard/page.tsx
+++ app/onboard/page.tsx
@@ -5,7 +5,7 @@ export default function Onboard() {
   const router = useRouter();
   return (
     <>
-      <button className="mb-4 px-6 py-2 bg-pink-200 text-white rounded-lg border border-pink-300 text-base">Login</button>
+      <button className="mb-4 px-8 py-2 bg-pink-200 text-white rounded-lg border border-pink-300 text-base" onClick={() => router.push('/onboard/login')}>Login</button>
       <button className="px-6 py-2 bg-pink-300 text-white rounded-lg border border-pink-400 text-base" onClick={() => router.push('/onboard/signup')}>Sign Up</button>
     </>
   );
